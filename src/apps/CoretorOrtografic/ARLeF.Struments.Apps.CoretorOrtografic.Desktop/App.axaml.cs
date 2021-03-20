ae0b4bd5ffdd7fd0ffedcd245dcee0fbff69603c
Massimo Romanin <dev@massimoromanin.com>
2021-03-20 13:53:20 +0100

    Implement AutoFac dependency injection strategy for Avalonia project;

--- src/apps/CoretorOrtografic/ARLeF.Struments.Apps.CoretorOrtografic.Desktop/App.axaml.cs
+++ src/apps/CoretorOrtografic/ARLeF.Struments.Apps.CoretorOrtografic.Desktop/App.axaml.cs
@@ -1,8 +1,10 @@
 using ARLeF.Struments.Apps.CoretorOrtografic.Desktop.ViewModels;
 using ARLeF.Struments.Apps.CoretorOrtografic.Desktop.Views;
+using ARLeF.Struments.Components.CoretorOrtografic.Core.SpellChecker;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Splat;
 
 namespace ARLeF.Struments.Apps.CoretorOrtografic.Desktop
 {
@@ -15,13 +17,10 @@ namespace ARLeF.Struments.Apps.CoretorOrtografic.Desktop
 
         public override void OnFrameworkInitializationCompleted()
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            new MainWindow
             {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
-            }
+                DataContext = new MainWindowViewModel(Locator.GetLocator().GetService<ISpellChecker>())
+            }.Show();
 
             base.OnFrameworkInitializationCompleted();
         }
