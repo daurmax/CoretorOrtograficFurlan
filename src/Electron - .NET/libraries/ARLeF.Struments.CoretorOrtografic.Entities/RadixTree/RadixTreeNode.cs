09511a3699749fe80d7ac3f586e7622e9c8294b7
Massimo Romanin <dev@massimoromanin.com>
2022-02-27 15:13:46 +0100

    Import RadixTreeNode.cs, general folder cleaning

---
+++ src/Electron - .NET/libraries/ARLeF.Struments.CoretorOrtografic.Entities/RadixTree/RadixTreeNode.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace ARLeF.Struments.CoretorOrtografic.Entities.RadixTree
+{
+    public class RadixTreeNode
+    {
+        private string _radixFilePath;
+        private int _edgePosition;
+
+        public RadixTreeNode(string radixTreeFilePath, int edgePosition)
+        {
+            _radixFilePath = radixTreeFilePath;
+            _edgePosition = edgePosition;
+        }
+
+        //public RadixTreeNode GetNextEdge()
+        //{
+        //    if (EdgesNumber <= NextNumber)
+        //    {
+        //        return new RadixTreeNode();
+        //    }
+        //}
+
+        public int EdgePosition
+        {
+            get
+            {
+                return _edgePosition;
+            }
+        }
+        public byte EdgesNumber
+        {
+            get
+            {
+                return RetrieveEdgeByte(_edgePosition);
+            }
+        }
+        public int NextPosition
+        {
+            get
+            {
+                return _edgePosition + 1;
+            }
+        }
+        public byte NextNumber
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        private byte RetrieveEdgeByte(int position)
+        {
+            FileStream radixTreeFileStream = File.OpenRead(_radixFilePath);
+
+            using (BinaryReader reader = new BinaryReader(radixTreeFileStream))
+            {
+                reader.BaseStream.Seek(_edgePosition + (int)position, SeekOrigin.Begin);
+
+                //Console.WriteLine($"Byte array position: [{reader.BaseStream.Position}]");
+                //Console.WriteLine($"Byte array value: [{reader.ReadByte()}]");
+
+                //reader.Read(test, i, 10);
+                return reader.ReadByte();
+            }
+        }
+    }
+}
---
+++ src/Electron - .NET/tests/ARLeF.Struments.CoretorOrtografic.Tests/Infrastructure/RadixTree/RadixTreeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ARLeF.Struments.CoretorOrtografic.Tests.Infrastructure.RadixTree
+{
+    public class RadixTreeTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
