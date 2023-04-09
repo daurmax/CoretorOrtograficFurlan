45374a3fab95010b68153b778560c7129fe7c604
Massimo Romanin <dev@massimoromanin.com>
2023-04-09 02:22:42 +0200

    fix(): fix wrong dictionaries path, implement copy of said dictionaries after building the library

--- Source/DotNet/Libraries/ARLeF.Struments.CoretorOrtografic.Dictionaries/Constants/DictionaryFilePaths.cs
+++ Source/DotNet/Libraries/ARLeF.Struments.CoretorOrtografic.Dictionaries/Constants/DictionaryFilePaths.cs
@@ -3,9 +3,9 @@ namespace ARLeF.Struments.CoretorOrtografic.Dictionaries.Constants
 {
     public static class DictionaryFilePaths
     {
-        public static readonly string SQLITE_ELISIONS_DATABASE_FILE_PATH = "Dictionaries/Elisions/SQLite/words.db";
-        public static readonly string SQLITE_ERRORS_DATABASE_FILE_PATH = "Dictionaries/Errors/SQLite/words.db";
-        public static readonly string SQLITE_FREC_DATABASE_FILE_PATH = "Dictionaries/Frec/SQLite/words.db";
+        public static readonly string SQLITE_ELISIONS_DATABASE_FILE_PATH = "Dictionaries/Elisions/SQLite/elisions.db";
+        public static readonly string SQLITE_ERRORS_DATABASE_FILE_PATH = "Dictionaries/Errors/SQLite/errors.db";
+        public static readonly string SQLITE_FREC_DATABASE_FILE_PATH = "Dictionaries/Frec/SQLite/frec.db";
         public static readonly string SQLITE_WORDS_DATABASE_FILE_PATH = "Dictionaries/WordsDatabase/SQLite/words.db";
         public static readonly string WORDS_RADIX_TREE_FILE_PATH = "Dictionaries/WordsRadixTree/words.rt";
     }
