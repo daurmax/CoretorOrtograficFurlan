26663f3fa284b8090400fd6680a7270da1883a20
Massimo Romanin <dev@massimoromanin.com>
2024-12-30 21:39:25 +0100

    feat(): upgrade Angular dependencies to version 18 and refactor configuration files

--- Source/Frontend/coretor-ortografic-furlan/src/app/app.module.ts
+++ Source/Frontend/coretor-ortografic-furlan/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { EditorComponent } from './components/editor/editor.component';
@@ -19,28 +19,21 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    EditorComponent,
-    SuggestionsModalComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    CKEditorModule,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    }),
-    EditorModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        EditorComponent,
+        SuggestionsModalComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        CKEditorModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        }),
+        EditorModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
